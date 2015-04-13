e3b8a944f4a52a9dc3b4fd06ee24e29416fb4b2a
David Lukes <dafydd.lukes@gmail.com>
2015-04-13 12:16:23 +0200

    stub out script for batch converting a corpus

---
+++ bin/elan2paula.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Transform a corpus of ELAN transcripts to the PAULA format.
+
+# Usage: elan2paula.sh in_dir
+
+SCRIPTDIR=$( cd "$( dirname "$0" )" && pwd )
+BASEDIR="$SCRIPTDIR/.."
+XSLTPROC="saxonb-xslt -ext:on"
+
+for file in $1/*.eaf; do
+    for template in "$BASEDIR/src/*.xsl"; do
+        comm="$XSLTPROC ""$file ""$template"
+        echo "Running: $comm"
+        $comm
+    done
+done
