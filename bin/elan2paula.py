276b1d4f087fd0b0826bcb61fac99830e5bbd514
David Lukes <dafydd.lukes@gmail.com>
2015-04-14 12:02:59 +0200

    switch preprocessing to manually created tempfile

--- bin/elan2paula.py
+++ bin/elan2paula.py
@@ -8,7 +8,6 @@ import os
 import sys
 import glob
 import subprocess
-import tempfile
 
 import re
 
@@ -16,15 +15,12 @@ SCRIPTDIR = os.path.dirname(os.path.realpath(__file__))
 BASEDIR = os.path.normpath(os.path.join(SCRIPTDIR, ".."))
 TEMPLDIR = os.path.join(BASEDIR, "src", "templates")
 LIBDIR = os.path.join(BASEDIR, "src", "lib")
-TEMPFILE = tempfile.NamedTemporaryFile()
+PREPROC = os.path.join(LIBDIR, "preprocess.xsl")
 IN_DIR = sys.argv[1]
 OUT_DIR = "elan-corpus"
 ACCEPTED_FILE_GLOB = "*.eaf"
 XSLTPROC = ["saxonb-xslt", "-ext:on"]
 # XSLTPROC = ["saxon", "-ext:on"]
-PREPROC = (XSLTPROC +
-           ["-xsl:{}".format(os.path.join(LIBDIR, "preprocess.xsl")),
-            "-o:{}".format(TEMPFILE.name)])
 XMLLINT = ["xmllint", "--valid", "--noout"]
 
 for f in glob.iglob(os.path.join(IN_DIR, ACCEPTED_FILE_GLOB)):
@@ -35,16 +31,28 @@ for f in glob.iglob(os.path.join(IN_DIR, ACCEPTED_FILE_GLOB)):
                          "before proceeding.\n".format(basename))
         sys.exit(1)
 
+    # create output directory for current document
+    file_no_ext = os.path.splitext(basename)[0]
+    curr_doc_out_dir = os.path.join(OUT_DIR, file_no_ext)
+    if not os.path.exists(curr_doc_out_dir):
+        os.makedirs(curr_doc_out_dir)
+
     # preprocess ELAN file (= deduplicate TIME_ORDER and rewire TIME_SLOT_REFs)
-    command = PREPROC + [f]
+    tempfile = os.path.join(OUT_DIR, file_no_ext, file_no_ext + ".temp")
+    command = XSLTPROC + ["-xsl:{}".format(PREPROC),
+                          "-o:{}".format(tempfile),
+                          f]
     sys.stderr.write("Preprocessing file {} with: {}\n".
                      format(basename, " ".join(command)))
     subprocess.call(command)
 
     for template in glob.iglob(os.path.join(TEMPLDIR, "*.xsl")):
-        command = XSLTPROC + [TEMPFILE.name, template]
+        command = XSLTPROC + [tempfile, template]
         sys.stderr.write("Running: {}\n".format(" ".join(command)))
         subprocess.call(command)
+
+    sys.stderr.write("Removing temporary files for {}.\n".format(basename))
+    os.remove(tempfile)
 
 sys.stderr.write("Verifying generated files according to DTDs:\n")
 for root, dirs, files in os.walk(OUT_DIR):
@@ -57,5 +65,3 @@ for root, dirs, files in os.walk(OUT_DIR):
             else:
                 sys.stderr.write(" ERROR\n")
                 sys.exit(1)
-
-TEMPFILE.close()
